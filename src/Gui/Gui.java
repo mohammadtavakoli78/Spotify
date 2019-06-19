155faa92b24a1c9bde3987820903b9d43376db1f
9731014 <9731014@ceit.local>
2019-06-19 10:45:46 +0430

    Gui and SouthPanel Classes modified.

--- src/Gui/Gui.java
+++ src/Gui/Gui.java
@@ -1,4 +1,24 @@
 package Gui;
 
-public class Gui {
+import javax.swing.*;
+import java.awt.*;
+
+public class Gui extends JFrame {
+    public Gui(){
+        super();
+        setSize(1600,1000);
+        setLayout(new BorderLayout());
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int width=(int)screenSize.getWidth();
+        int height=(int)screenSize.getHeight();
+        setLocation(width/2-800,height/2-500);
+        SouthPanel southPanel=new SouthPanel();
+        add(southPanel,BorderLayout.SOUTH);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Gui gui=new Gui();
+    }
 }
--- src/Gui/SouthPanel.java
+++ src/Gui/SouthPanel.java
@@ -1,4 +1,28 @@
 package Gui;
 
-public class SouthPanel {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SouthPanel extends JPanel {
+    public SouthPanel(){
+
+        super();
+        setLayout(new BorderLayout());
+        setBackground(new Color(0x14000000, true));
+
+        JPanel playerButtons=new JPanel();
+        JPanel soundManager=new JPanel();
+        JPanel buttons=new JPanel();
+        JPanel progressBar=new JPanel();
+
+        playerButtons.setLayout(new BorderLayout());
+        buttons.setLayout(new FlowLayout(FlowLayout.CENTER));
+        JButton playButton=new JButton();
+        playButton.setIcon(new ImageIcon("C:\\Users\\Win 1809 UEFI\\Desktop\\play.jpg"));
+        buttons.add(playButton);
+        playerButtons.add(buttons);
+        add(playButton,BorderLayout.CENTER);
+    }
 }
