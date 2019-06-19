30f1049d3db35a59c1036f8ac08b96078f1ee6ef
9731014 <9731014@ceit.local>
2019-06-19 11:00:30 +0430

    playMusic and nextMusic buttons added to SouthPanel.

--- src/Gui/SouthPanel.java
+++ src/Gui/SouthPanel.java
@@ -10,7 +10,8 @@ public class SouthPanel extends JPanel {
 
         super();
         setLayout(new BorderLayout());
-        setBackground(new Color(0x14000000, true));
+        setForeground(new Color(0x41040304, true));
+//        setBackground(new Color(0x14000000, true));
 
         JPanel playerButtons=new JPanel();
         JPanel soundManager=new JPanel();
@@ -19,10 +20,13 @@ public class SouthPanel extends JPanel {
 
         playerButtons.setLayout(new BorderLayout());
         buttons.setLayout(new FlowLayout(FlowLayout.CENTER));
-        JButton playButton=new JButton();
-        playButton.setIcon(new ImageIcon("C:\\Users\\Win 1809 UEFI\\Desktop\\play.jpg"));
-        buttons.add(playButton);
-        playerButtons.add(buttons);
-        add(playButton,BorderLayout.CENTER);
+        JButton playMusic=new JButton();
+        JButton nextMusic=new JButton();
+        playMusic.setIcon(new ImageIcon("C:\\Users\\Win 1809 UEFI\\Desktop\\play.png"));
+        nextMusic.setIcon(new ImageIcon("C:\\Users\\Win 1809 UEFI\\Desktop\\next.png"));
+        buttons.add(nextMusic);
+        buttons.add(playMusic);
+        playerButtons.add(buttons,BorderLayout.NORTH);
+        add(playerButtons,BorderLayout.CENTER);
     }
 }
